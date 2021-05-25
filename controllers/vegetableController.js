0f63e6f16bce444e8b704166616db72c55450172
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-05-25 10:07:16 +0530

    Added sort for vegetables data

--- controllers/vegetableController.js
+++ controllers/vegetableController.js
@@ -1,6 +1,6 @@
 var Vegetable = require('../models/vegetablesModel');
 
 exports.getVegetable = async (req, res) => {
-    const vegetable = await Vegetable.find();
+    const vegetable = await Vegetable.find().sort({"name": 1});
     res.json(vegetable);
 }
