8b7769fd7580a7c5e32f26ef4cdf42b22b01f51a
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-06-11 08:17:53 +0530

    added fruits

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const express = require('express'),
     cors = require('cors');
 require('dotenv').config();
 
+var userRoute = require('./route/userRoute');
+var vegetableRoute = require('./route/vegetableRoute');
+var recipeRoute = require('./route/recipeRoute');
+var fruitRoute = require('./route/fruitsRoute');
+var allIngredientRoute = require('./route/allIngredientsRoute');
+
 const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
     console.log("Database connected successfully")
@@ -21,13 +27,11 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var userRoute = require('./route/userRoute');
-var vegetableRoute = require('./route/vegetableRoute');
-var recipeRoute = require('./route/recipeRoute');
-
 userRoute(app);
 vegetableRoute(app);
 recipeRoute(app);
+fruitRoute(app);
+allIngredientRoute(app);
 
 app.use((req, res) => {
     res.status(404).send({ url: req.originalUrl + ' not found' });
