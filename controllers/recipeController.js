8bdbc8c65e47a484f0f8cc9cbee693f83c8dd0ac
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-06-27 22:00:53 +0530

    added try/catch

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,13 +2,17 @@ var jwt = require("jsonwebtoken");
 var Recipe = require('../models/recipeModel');
 
 exports.postRecipe = async (req, res) => {
-    const recipe = new Recipe(req.body);
-    recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
-    // const token = req.token;
-    // const decoded = jwt.verify(token, "RESTFULAPIs");
-    // recipe.author = {id: decoded._id, username: decoded.username, phone: decoded.phone};
-    recipe.save();
-    res.json({success: true});
+    try {
+        const recipe = new Recipe(req.body);
+        recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
+        // const token = req.token;
+        // const decoded = jwt.verify(token, "RESTFULAPIs");
+        // recipe.author = {id: decoded._id, username: decoded.username, phone: decoded.phone};
+        recipe.save();
+        res.json({success: true});
+    } catch (error) {
+        res.json(error);
+    }
 }
 
 exports.getRecipe = async (req, res) => {
