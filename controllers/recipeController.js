1bcfdf906784d33d94a9d2d53e8ada44ea8aabf5
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-06-28 08:11:54 +0530

    added parse

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -5,7 +5,7 @@ exports.postRecipe = async (req, res) => {
     try {
         const recipe = new Recipe(req.body);
         recipe.recipeImage = req.files.recipeImage.map(f => ({ url: f.path, filename: f.filename, type: f.mimetype, size: f.size }));
-        console.log(JSON.parse(req.body.ingredients));
+        console.log(req.body);
         // recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
         // const token = req.token;
         // const decoded = jwt.verify(token, "RESTFULAPIs");
