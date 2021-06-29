3d14f54dcc0baec2ab374405bd8c99449effc270
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-06-29 17:18:22 +0530

    changed model

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -13,8 +13,7 @@ exports.postRecipe = async (req, res) => {
             var preparationObj = JSON.parse(preparation);
             return preparationObj;
         })
-        console.log(req.files)
-        // recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
+        recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
         // const token = req.token;
         // const decoded = jwt.verify(token, "RESTFULAPIs");
         // recipe.author = {id: decoded._id, username: decoded.username, phone: decoded.phone};
