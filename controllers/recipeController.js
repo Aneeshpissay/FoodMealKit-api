c4c102e3166a569fe444503303da44c2780da1e2
Aneesh Pissay <33913608+Aneeshpissay@users.noreply.github.com>
2021-08-07 21:42:39 +0530

    added video support

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -18,7 +18,7 @@ exports.postRecipe = async (req, res) => {
             return preparationObj;
         })
         if(req.files.recipeVideo) {
-            recipe.recipeVideo = req.files.recipeVideo.map(f => ({ url: f.path, filename: f.filename, type: f.mimetype, size: f.size }));
+            recipe.recipeVideo = {url: req.files.recipeVideo[0].path, filename: req.files.recipeVideo[0].filename, type: req.files.recipeVideo[0].mimetype, size: req.files.recipeVideo[0].size};
         }
         const usertoken = req.headers['authorization'];
         const token = usertoken.split(' ');
